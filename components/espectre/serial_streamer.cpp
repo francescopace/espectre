c3b1efe280bc5120ded1f6631caf9a44049ff7fe
Francesco Pace <francesco.pace@gmail.com>
2025-12-29 12:59:06 +0100

    fix: disable SerialStreamer when logger uses hardware UART
    When ESPHome is configured with hardware_uart: UART0 (common on boards
    with external USB-UART chips like CH343), the USB Serial JTAG driver
    is not initialized. Calling usb_serial_jtag_read_bytes() in this case
    causes a crash (Guru Meditation Error: Load access fault).
    
    This fix checks for USE_LOGGER_USB_SERIAL_JTAG before enabling the
    SerialStreamer. If the logger is not using USB Serial JTAG, the
    streamer is disabled with an appropriate log message.
    
    See: https://github.com/francescopace/espectre/issues/48#issuecomment-3694559082

--- components/espectre/serial_streamer.cpp
+++ components/espectre/serial_streamer.cpp
@@ -17,7 +17,11 @@
 #include "sdkconfig.h"
 
 // USB Serial JTAG is only available on ESP32-C3, C6, S2, S3, H2
-#if defined(CONFIG_SOC_USB_SERIAL_JTAG_SUPPORTED) && CONFIG_SOC_USB_SERIAL_JTAG_SUPPORTED
+// AND only when ESPHome logger is configured to use USB Serial JTAG (default)
+// If user sets hardware_uart: UART0, the USB Serial JTAG driver won't be initialized
+// and calling usb_serial_jtag_read_bytes() will crash.
+// See: https://github.com/francescopace/espectre/issues/48
+#if defined(CONFIG_SOC_USB_SERIAL_JTAG_SUPPORTED) && CONFIG_SOC_USB_SERIAL_JTAG_SUPPORTED && defined(USE_LOGGER_USB_SERIAL_JTAG)
 #include "driver/usb_serial_jtag.h"
 #define USE_USB_SERIAL_JTAG 1
 #else
@@ -35,7 +39,12 @@ void SerialStreamer::init() {
   cmd_buffer_index_ = 0;
   memset(cmd_buffer_, 0, CMD_BUFFER_SIZE);
 #if !USE_USB_SERIAL_JTAG
+#if defined(CONFIG_SOC_USB_SERIAL_JTAG_SUPPORTED) && CONFIG_SOC_USB_SERIAL_JTAG_SUPPORTED
+  // Chip supports USB Serial JTAG but ESPHome is using hardware UART for logging
+  ESP_LOGD(TAG, "Serial streaming disabled (logger using hardware UART)");
+#else
   ESP_LOGW(TAG, "USB Serial JTAG not available on this chip - serial streaming disabled");
+#endif
 #endif
 }
 
